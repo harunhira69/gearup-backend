c0030515cd1fb0d6ca4ede390018813ac646c6b9
Harun <harunabhi4@gmail.com>
2026-07-16 16:25:55 +0600

    successfully deploy

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,4 @@
-import app from '../src/app';
-import { prisma } from '../src/lib/prisma';
+// @ts-ignore
+import app from "../dist/server.js";
 
-declare global {
-  var __prismaConnected: boolean | undefined;
-}
-
-export default async function handler(req: any, res: any) {
-  try {
-    if (!globalThis.__prismaConnected && process.env.DATABASE_URL) {
-      await prisma.$connect();
-      globalThis.__prismaConnected = true;
-    }
-  } catch (error) {
-    console.error('Prisma connection failed in Vercel handler:', error);
-  }
-
-  return app(req, res);
-}
+export default app;
